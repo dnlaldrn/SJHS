6d4725c549adebf36f92770c6a0d373b958daf2a
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2025-07-22 13:01:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
             document.querySelector('.sidebar').style.display = "flex";
             document.querySelector('.homeSection').style.display = "none";
             document.querySelector('.strandSection').style.display = "none";
-             document.querySelector('.featured-section').style.display = "none";
+            document.querySelector('.featured-section').style.display = "none";
+            document.querySelector('.enrollment-section').style.display = "none";
+             document.querySelector('.contact-section').style.display = "none";
+
 
         })
         document.querySelector('.hideButton').addEventListener("click", (event)=>{
             event.preventDefault();
             document.querySelector('.sidebar').style.display = "none";
+        })
+
+        document.querySelector('.send-button').addEventListener("click", (event)=>{
+            alert('This button is undermaintenance')
+            event.preventDefault();
+
         })
     
