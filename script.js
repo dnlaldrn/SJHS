f488d1b73ae76ddae59d40309c22e3702a2be900
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2025-07-22 13:19:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,12 @@
         document.querySelector('.hideButton').addEventListener("click", (event)=>{
             event.preventDefault();
             document.querySelector('.sidebar').style.display = "none";
+             document.querySelector('.homeSection').style.display = "block";
+             document.querySelector('.strandSection').style.display = "block";
+            document.querySelector('.featured-section').style.display = "block";
+            document.querySelector('.enrollment-section').style.display = "block";
+             document.querySelector('.contact-section').style.display = "block";
+
         })
 
         document.querySelector('.send-button').addEventListener("click", (event)=>{
@@ -18,4 +24,62 @@
             event.preventDefault();
 
         })
+        
+        document.querySelector('.home').addEventListener("click", ()=>{
+            document.querySelector('.sidebar').style.display = "none";
+            document.querySelector('.homeSection').style.display = "block";
+             document.querySelector('.strandSection').style.display = "block";
+            document.querySelector('.featured-section').style.display = "block";
+            document.querySelector('.enrollment-section').style.display = "block";
+             document.querySelector('.contact-section').style.display = "block";
+
+
+        })
+
+              document.querySelector('.aboutus').addEventListener("click", ()=>{
+            document.querySelector('.sidebar').style.display = "none";
+            document.querySelector('.homeSection').style.display = "block";
+             document.querySelector('.strandSection').style.display = "block";
+            document.querySelector('.featured-section').style.display = "block";
+            document.querySelector('.enrollment-section').style.display = "block";
+             document.querySelector('.contact-section').style.display = "block";
+
+
+        })
+    
+              document.querySelector('.strandsoffered').addEventListener("click", ()=>{
+            document.querySelector('.sidebar').style.display = "none";
+            document.querySelector('.homeSection').style.display = "block";
+             document.querySelector('.strandSection').style.display = "block";
+            document.querySelector('.featured-section').style.display = "block";
+            document.querySelector('.enrollment-section').style.display = "block";
+             document.querySelector('.contact-section').style.display = "block";
+
+
+        })
+    
+
+              document.querySelector('.enroll').addEventListener("click", ()=>{
+            document.querySelector('.sidebar').style.display = "none";
+            document.querySelector('.homeSection').style.display = "block";
+             document.querySelector('.strandSection').style.display = "block";
+            document.querySelector('.featured-section').style.display = "block";
+            document.querySelector('.enrollment-section').style.display = "block";
+             document.querySelector('.contact-section').style.display = "block";
+
+
+        })
+    
+
+              document.querySelector('.contactus').addEventListener("click", ()=>{
+            document.querySelector('.sidebar').style.display = "none";
+            document.querySelector('.homeSection').style.display = "block";
+             document.querySelector('.strandSection').style.display = "block";
+            document.querySelector('.featured-section').style.display = "block";
+            document.querySelector('.enrollment-section').style.display = "block";
+             document.querySelector('.contact-section').style.display = "block";
+
+
+        })
+    
     
